9bb6f92a13b1d5ecf315f25a2fc5d05d3d9218ab
Samuel Henrique <150966678+zyypj@users.noreply.github.com>
2024-05-29 01:25:44 -0300

    First version

--- src/main/java/br/com/pulse/clans/Main.java
+++ src/main/java/br/com/pulse/clans/Main.java
@@ -8,33 +8,44 @@ import br.com.pulse.clans.util.ClanManager;
 import br.com.pulse.clans.util.placeholder.PlaceholderSupport;
 import com.tomkeuper.bedwars.api.BedWars;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
     private ClanManager clanManager;
-    BedWars bedwarsAPI = Bukkit.getServicesManager().getRegistration(BedWars.class).getProvider();
+    private BedWars bedwarsAPI;
 
     @Override
     public void onEnable() {
-        clanManager = new ClanManager(this);
         if (Bukkit.getPluginManager().getPlugin("BedWars2023") == null) {
-            getLogger().severe("BedWars2023 was not found. Disabling...");
+            getLogger().severe("BedWars2023 não foi encontrado. Desabilitando...");
             Bukkit.getPluginManager().disablePlugin(this);
             return;
         }
+
+        // Obtém o provedor da API BedWars
+        RegisteredServiceProvider<BedWars> rsp = Bukkit.getServicesManager().getRegistration(BedWars.class);
+        if (rsp == null || rsp.getProvider() == null) {
+            getLogger().severe("API BedWars não encontrada. Desabilitando...");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+        bedwarsAPI = rsp.getProvider();
+
+        clanManager = new ClanManager(this);
         BedWars2023 bedWars2023 = new BedWars2023(this, clanManager);
         bedwarsAPI.getAddonsUtil().registerAddon(bedWars2023);
 
         if (Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
             Bukkit.getScheduler().runTaskLater(this, () ->
-                    getLogger().info("Hook to PlaceholderAPI support!"), 20L);
+                    getLogger().info("Hook para suporte ao PlaceholderAPI!"), 20L);
             new PlaceholderSupport(clanManager).register();
         }
 
         if (Bukkit.getPluginManager().isPluginEnabled("LeafPlugins") || Bukkit.getPluginManager().isPluginEnabled("LeafPunish")) {
             Bukkit.getScheduler().runTaskLater(this, () ->
-                    getLogger().info("Hook to LeafPlugins support!"), 20L);
+                    getLogger().info("Hook para suporte ao LeafPlugins!"), 20L);
             Bukkit.getPluginManager().registerEvents(new RankedEvents(this, clanManager), this);
         }
 
@@ -44,7 +55,7 @@ public final class Main extends JavaPlugin {
         getCommand("cc").setExecutor(new ClanChatCommand(clanManager));
 
         Bukkit.getScheduler().runTaskLater(this, () ->
-                getLogger().info("&aPulseClans enable!"), 30L);
+                getLogger().info("&aPulseClans habilitado!"), 30L);
 
     }
 
